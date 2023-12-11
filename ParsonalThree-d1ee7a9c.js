dd18e4c654a01498e4f2a5963e3ba8fe12a0c7b8
takitoAbe <41960273+appulegugure@users.noreply.github.com>
2023-12-11 11:40:08 +0900

    Delete ParsonalThree-d1ee7a9c.js

--- ParsonalThree-d1ee7a9c.js
+++
@@ -1,1 +0,0 @@
-import{d as a,r as t,o as n,c as o,t as r}from"./index-87f051a4.js";const _=a({__name:"ParsonalThree",setup(s){const e=t("3");return(c,l)=>(n(),o("h1",null,r(e.value),1))}});export{_ as default};
